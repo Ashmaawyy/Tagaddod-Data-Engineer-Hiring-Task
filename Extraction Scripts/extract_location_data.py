beefccd09a7feee2874c05b8f937501e2adee280
Ashmaawyy <al_ashmawy@outlook.com>
2022-08-06 11:09:38 +0200

    The fields variable is no longer needed ; )

--- Extraction Scripts/extract_location_data.py
+++ Extraction Scripts/extract_location_data.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 json_file = open('./Data/Raw Data/tagaddod-d8ffe--MsZkGFSCtYxntenMuVF-export.json')
 json_data_dict = json.load(json_file)
-fields = ['collector_id', 'device_id', 'latitude', 'longitude', 'snapshot_datetime']
 
 json_data_df = pd.DataFrame.from_dict(json_data_dict, orient = 'index')
 json_data_df.to_csv(r'./Data/Transformed Data/Tagaddod_json_data.csv', index = False, header = True)
