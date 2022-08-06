2b75fbe0f20df8d92c5d3721cfa8937dfa3b0023
Ashmaawyy <al_ashmawy@outlook.com>
2022-08-06 11:32:20 +0200

    Removed unused imports ; )

--- Extraction Scripts/extract_location_data.py
+++ Extraction Scripts/extract_location_data.py
@@ -1,6 +1,5 @@
 import json
-import pprint
-import pandas as pd
+from pandas import DataFrame
 
 import_dir = r'./Data/Raw Data/tagaddod-d8ffe--MsZkGFSCtYxntenMuVF-export.json'
 export_dir = r'./Data/Transformed Data/Tagaddod_json_data.csv'
@@ -8,7 +7,7 @@ export_dir = r'./Data/Transformed Data/Tagaddod_json_data.csv'
 def import_json_data_into_df(import_dir: str) -> dict:
     json_file = open(import_dir)
     json_data_dict = json.load(json_file)
-    return pd.DataFrame.from_dict(json_data_dict, orient = 'index')
+    return DataFrame.from_dict(json_data_dict, orient = 'index')
 
 def export_json_data_into_csv(export_dir: str):
     import_json_data_into_df(import_dir).to_csv(export_dir, index = False, header = True)
