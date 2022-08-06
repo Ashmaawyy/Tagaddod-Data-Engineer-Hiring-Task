358c1dfa3842af86d51595e7b6826c5977619ca3
Ashmaawyy <al_ashmawy@outlook.com>
2022-08-06 11:29:14 +0200

    Code is now more clean ; )

--- Extraction Scripts/extract_location_data.py
+++ Extraction Scripts/extract_location_data.py
@@ -2,8 +2,13 @@ import json
 import pprint
 import pandas as pd
 
-json_file = open('./Data/Raw Data/tagaddod-d8ffe--MsZkGFSCtYxntenMuVF-export.json')
-json_data_dict = json.load(json_file)
+import_dir = r'./Data/Raw Data/tagaddod-d8ffe--MsZkGFSCtYxntenMuVF-export.json'
+export_dir = r'./Data/Transformed Data/Tagaddod_json_data.csv'
 
-json_data_df = pd.DataFrame.from_dict(json_data_dict, orient = 'index')
-json_data_df.to_csv(r'./Data/Transformed Data/Tagaddod_json_data.csv', index = False, header = True)
+def import_json_data_into_df(import_dir: str) -> dict:
+    json_file = open(import_dir)
+    json_data_dict = json.load(json_file)
+    return pd.DataFrame.from_dict(json_data_dict, orient = 'index')
+
+def export_json_data_into_csv(export_dir: str):
+    import_json_data_into_df(import_dir).to_csv(export_dir, index = False, header = True)
